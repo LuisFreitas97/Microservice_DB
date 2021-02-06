b5c38f502e32ff6eb12263e4346c873bf055030a
LuisFreitas97 <lgf1997@outlook.pt>
2021-02-06 19:56:59 +0000

    inital commit.

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors  from "cors";
+import { DbConfig } from './app/config/db.config.js';
+import {router} from './app/routes/web.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+/*app.get("/", (req, res) => {
+  res.json({ message: "Welcome to application." });
+});*/
+
+app.use('/',router);
+
+// make db connection
+var db = new DbConfig();
+db.connectToDB();
+
+// set port, listen for requests
+const PORT = process.env.SERVICE_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
