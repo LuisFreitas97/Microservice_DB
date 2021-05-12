de7fb87aa1337ca4ca953524515fdf22bca10ce8
LuisFreitas97 <lgf1997@outlook.pt>
2021-05-12 11:16:38 +0100

    Aumentar o payload para conseguir inserir grandes quantidades de dados.

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use('/',router);
 
